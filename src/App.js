8833b20f9b2c41754081cb7d72a77e5ee0b24cb0
AudreyChigarira <audreymandy096@gmail.com>
2022-01-15 14:00:53 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./components/Posts/Posts.css";
 
-function App() {
+import Post from "./components/Posts/Posts.jsx";
+  
+const Posts = () => {
+  const myblogPosts = [
+    
+    {
+      title: "REACT?",
+
+      body: `
+      React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. 
+      It is maintained by Meta and a community of individual developers and companies. 
+      React can be used as a base in the development of single-page or mobile applications`,
+
+      author: "Audrey Chigarira",
+      
+      imgUrl:
+        "https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png",
+      
+    },
+   
+    
+  ];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="posts-container">
+      {myblogPosts.map((post, index) => (
+        <Post key={index} index={index} post={post} />
+      ))}
     </div>
   );
-}
-
-export default App;
+};
+  
+export default Posts;
